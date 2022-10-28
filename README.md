0842356ace5fb90dcac62809b9906e0033ab1834
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-10-28 10:29:50 -0500

    create a function that iterates through an array and replaces 3 with Wont you be my neighbor? "

--- README.md
+++ README.md
@@ -1,5 +1,11 @@
-Describe:
+Describe: transformToStringArr()
 
 Test: "It should return an array of numbers in strings from 0 to the user's inputted number"
 Code: transformToStringArr(5);
 Expected Output: ["0", "1", "2", "3", "4", "5"]
+
+Describe: beepBoop()
+
+Test: "For numbers that contain a 3, all digits are replaced with "Won't you be my neighbor?". Else it's just a number"
+Code: beepBoop(3)
+Expected Output: ["0", "1", "2", "Won't you be my neighbor?"]
--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,28 @@
+// Business logic
+
 const userInput = 7;
 let stringArr = [];
-
 function transformToStringArr (number) {
   for(let i = 0; i <= number; i++){
     stringArr.push(i.toString());
   }
 }
 
-beepBoop()
+transformToStringArr (userInput);
+// For numbers that contain a 1, all digits are replaced with "Beep!"
+// For example, all digits of the number 109, 11, or 1 would be replaced with "Beep!"
+// For numbers that contain a 2, all digits are replaced with "Boop!"
+// For example, all digits of the number 2, 24, or 2099 would be replaced with "Boop!"
+// For numbers that contain a 3, all digits are replaced with "Won't you be my neighbor?"
+// For example, all digits of the number 39, 3, or 8763 would be replaced with "Won't you be my neighbor?"
+
+
+function beepBoop(arr) {
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i].includes("3") === true) {
+      console.log("Won't you be my neighbor?")
+    } else {
+      console.log(arr[i])
+    }
+  }
+}
