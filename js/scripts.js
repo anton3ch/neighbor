40a93e6d48d53551393ab31c1c2ba38e2be75f84
Anton Ch <43277455+anton3ch@users.noreply.github.com>
2022-10-28 19:15:42 -0500

    add functionality to the reset button

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
 // Business logic
 
-
 function transformToStringArr (number, arr) {
   for(let i = 0; i <= number; i++){
     arr.push(i.toString());
@@ -21,9 +20,6 @@ function beepBoop(arr) {
   }
 }
 
-
-
-
 //UI
 
 function createBeep(){
@@ -34,12 +30,15 @@ function createBeep(){
   ul.append(li);
 }
 function launchBeep(){
-  const img = document.createElement("img");
+  const img = document.getElementById("img1");
   document.body.append(img);
   img.setAttribute("src", "../img/beep.png");
   img.setAttribute("class", "ghost");
   img.setAttribute("width", "150px");
-  img.setAttribute("alt", "Beep!");
+  // img.setAttribute("alt", "Beep!");
+  setTimeout(function(){
+    img.setAttribute("class", "hidden");
+  }, 5000);
 }
 
 function createBoop(){
@@ -50,12 +49,15 @@ function createBoop(){
 }
 
 function launchBoop(){
-  const img = document.createElement("img");
+  const img = document.getElementById("img2");
   document.body.append(img);
   img.setAttribute("src", "../img/boop.png");
   img.setAttribute("class", "ghost2");
   img.setAttribute("width", "150px");
-  img.setAttribute("alt", "Boop!");
+  // img.setAttribute("alt", "Boop!");
+  setTimeout(function(){
+    img.setAttribute("class", "hidden");
+  }, 5000);
 }
 
 function createWontYouBeMyNeighbor(){
@@ -66,12 +68,16 @@ function createWontYouBeMyNeighbor(){
 }
 
 function launchWontYouBeMyNeighbor(){
-  const img = document.createElement("img");
+  const img = document.getElementById("img3");
   document.body.append(img);
   img.setAttribute("src", "../img/neighbor.png");
   img.setAttribute("class", "ghost3");
   img.setAttribute("width", "150px");
-  img.setAttribute("alt", "Beep!");
+  // img.setAttribute("alt", "Won't you be my neighbor?");
+  setTimeout(function(){
+    img.setAttribute("class", "hidden");
+  }, 10000);
+
 }
 
 function createNumber(number){
@@ -94,13 +100,12 @@ function handleForm(){
     let stringArr = [];
     transformToStringArr(userNum, stringArr);
     const h3 = document.getElementById("h3");
-    h3.append("Scary Halloween, " + userName + "!")
-    document.getElementById("output").prepend(h3);
+    h3.innerText = "Scary Halloween, " + userName + "!";
     beepBoop(stringArr);
     launchBeep();
     launchBoop();
     launchWontYouBeMyNeighbor();
-    
+    $('#output').show();
     document.getElementById("formNest").setAttribute("class", "animate__animated animate__fadeOut");
   });
 }
@@ -110,12 +115,21 @@ function handleForm(){
 
 window.addEventListener("load", function(){
   const start = document.getElementById("start");
+  const reset = document.getElementById("reset");
 
+  handleForm();
+    $('button#reset').click(function () {
+      $('#list').empty();
+      $('#output').hide();
+      document.getElementById("formNest").setAttribute("class", "animate__animated animate__fadeIn");
+  });
+    
+
+  // });
   start.addEventListener("click", function(){
     document.getElementById("formNest").setAttribute("class", "animate__animated animate__fadeIn");
     start.setAttribute("class", "hidden");
   });
-  handleForm();
 });
 
 
